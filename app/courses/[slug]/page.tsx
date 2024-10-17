665898abc2ab054a72999a557fde0654f7bb19db
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2024-10-18 00:01:44 +0600

    It changes the slug file

--- app/courses/[slug]/page.tsx
+++ app/courses/[slug]/page.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function page({params}:{params:{slug:string}}) {
   return (
-    <div>
+    <section className='w-full min-h-[90vh] bg-slate-100 dark:bg-slate-800 '>
+      <div className='w-[80%] min-h-[90vh] bg-slate-50 dark:bg-slate-900 mx-auto  py-8 px-4'>
+
+      </div>
       {params.slug}
-    </div>
+    </section>
   )
 }
 
